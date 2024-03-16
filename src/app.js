5a1c71d74f01f368aeddee4a45e19bead4efd006
Taylan <ferreirataylan123@gmail.com>
2024-03-16 08:51:00 -0300

    arquibos adicionados

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
 import routes from './routes.js';
 
 class App {
-
     constructor() {
         this.server = express();
+
+        mongoose.connect(
+            'mongodb+srv://devhouse:devhouse@devhouse.xstryl6.mongodb.net/?retryWrites=true&w=majority'
+        );
 
         this.middlewares();
         this.routes();
     }
 
     middlewares() {
+        this.server.use(cors());
+        this.server.use('/files', express.static(path.resolve('./uploads')));
+
         this.server.use(express.json());
     }
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from './app.js';
 
-app.listen(3333);   
+app.listen(3333, () => console.log('Api running at port 3333'));
